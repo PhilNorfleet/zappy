47f114a013e0b984766fdfafdac5916f4b5e930d
PhilNorfleet <norfleet.phillip@gmail.com>
2020-07-29 13:30:24 -0400

    initial commit, creates react app and installs bootstrap

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Container, Col, Row } from "react-bootstrap";
+function App() {
+  return (
+    <Container>
+      <Row className="row">
+        <Col>
+          Zappy Calculator
+        </Col>
+      </Row>
+    </Container>
+
+  );
+}
+
+export default App;
