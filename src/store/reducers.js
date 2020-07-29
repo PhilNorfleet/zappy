87181b127eb1409f2cc61a6b7f261dd3c33d8946
PhilNorfleet <norfleet.phillip@gmail.com>
2020-07-29 15:14:02 -0400

    installs redux and related packages, creates store

---
+++ src/store/reducers.js
@@ -0,0 +1,13 @@
+
+const initialState = {
+
+};
+
+const appReducer = (state = initialState, {type, payload}) => {
+    switch(type) {
+        default:
+            return state;
+    }
+}
+
+export default appReducer;
